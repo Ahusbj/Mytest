3cfb844320b56037b4cb80d352ba8026b4b2b7fc
Ahusbj <laianjht.com>
2020-06-16 16:52:31 +0800

    qwe_2

--- sort.cpp
+++ sort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-1223
-"coryright @ --Ahusbj--" 
+
+"coryright @ --Ahusbj--" mname
 void Qsort(int arr[], int low, int high){
     if (high <= low) return;
     int i = low;
